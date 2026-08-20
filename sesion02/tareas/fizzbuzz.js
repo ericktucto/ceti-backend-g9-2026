9f68c43c81d4b599ae15ba771b23fcb97dc91ae8
Erick Tucto <erick@ericktucto.com>
2026-08-19 19:18:02 -0500

    Solucion de tareas de sesion 2

---
+++ sesion02/tareas/fizzbuzz.js
@@ -0,0 +1,15 @@
+for (let index = 1; index <= 100; index++) {
+    if (index % 15 === 0) {
+        console.log("FizzBuzz");
+        continue;
+    }
+    if (index % 3 === 0) {
+        console.log("Fizz");
+        continue;
+    }
+    if (index % 5 === 0) {
+        console.log("Buzz");
+        continue;
+    }
+    console.log(index);
+}
